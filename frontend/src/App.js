c8e7a3ce741565a334dec19760128bfcb39fb614
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-06 10:49:14 -0400

    Add missing frontend code to support one chatroom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {messages:[]}
+    this.handleSendMessage = this.handleSendMessage.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.cableApp.messages = this.props.cableApp.cable.subscriptions.create({channel: "MessagesChannel", room: "One" },
+      {
+        received: (message) => this.setState({ messages: [message, ...this.state.messages,] })
+      })
+  }
+
+  handleSendMessage(e) {
+    e.preventDefault()
+    const message = document.getElementById('input').value
+    console.log(message)
+    this.props.cableApp.messages.send({content: message})
+  }
+
+  displayMessages() {
+    const messages = this.state.messages.map( message => <li>{message.content}</li> )
+    return (
+      <ul>
+        {messages}
+      </ul>
+    )
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <form onSubmit={this.handleSendMessage}>
+          <input id="input" type="text" />
+          <input type="submit" value="Send message" />
+        </form>
+
+        <div>
+          {this.displayMessages()}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
