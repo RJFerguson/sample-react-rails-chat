7a90838ba07fc98fae1333430047fca3053cfa07
curiositypaths <curiositypaths@users.noreply.github.com>
2017-07-06 09:53:02 -0400

    Add messages controller

---
+++ api/app/channels/messages_channel.rb
@@ -0,0 +1,9 @@
+class MessagesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chat_#{params[:room]}"
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast("chat_#{params[:room]}", data)
+  endd
+end
